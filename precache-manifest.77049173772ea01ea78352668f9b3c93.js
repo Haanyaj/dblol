b2e2e02820f95756424c9ee8c5ec8de747a52e47
Alvyn YANG <alvyn.yang@epitech.eu>
2023-04-05 00:23:47 +0200

    Updates

---
+++ precache-manifest.77049173772ea01ea78352668f9b3c93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e81b4c3974f4acf7f5c3b6167410ed",
+    "url": "/dblol/index.html"
+  },
+  {
+    "revision": "cf758545c43be6d2426c",
+    "url": "/dblol/static/css/main.0f077273.chunk.css"
+  },
+  {
+    "revision": "0f014faf4b02f835f23f",
+    "url": "/dblol/static/js/2.fd8e0e09.chunk.js"
+  },
+  {
+    "revision": "415a7dd1e59e0fc24b5b9fa25cf2fa2a",
+    "url": "/dblol/static/js/2.fd8e0e09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf758545c43be6d2426c",
+    "url": "/dblol/static/js/main.fe6a46a9.chunk.js"
+  },
+  {
+    "revision": "8cb2acede3f2ce1a0fe9",
+    "url": "/dblol/static/js/runtime-main.cb0e37e1.js"
+  },
+  {
+    "revision": "a14711158ac3b856d5bde4effbf5ba35",
+    "url": "/dblol/static/media/landscape.a1471115.png"
+  }
+]);
